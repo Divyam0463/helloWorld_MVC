4de6b540084aa9216f07d05abb4ce5189a6e1732
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-04-21 15:51:14 +0530

    implemented UC3

--- src/main/java/com/example/hello_world_mvc/Controller/helloWorldController.java
+++ src/main/java/com/example/hello_world_mvc/Controller/helloWorldController.java
@@ -1,11 +1,22 @@
 package com.example.hello_world_mvc.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 
 @RestController
 public class helloWorldController {
+
+  //UC3
+  @RequestMapping("/")
+  public String getHellofromMain() {
+    return "hello from bridgelabz" ; 
+  }  
+
+  //UC1
   @GetMapping("/hello")
   public String getHello() {
     return "hello world" ;
